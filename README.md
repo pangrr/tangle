f7581917ca340a27b3791358fdb43fe1563bba36
rpang27 <rpang27@bloomberg.net>
2019-03-18 12:42:43 -0400

    remove test folder

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # a literate programming tool to generate code from markdown files
 
 
-## how to install
-`npm install -g @pangrr/tangle`
 ## how to use
-`tangle foo.md`
+`node tangle foo.md`
--- test/code/update-words-familiarity.ts
+++
@@ -1,7 +0,0 @@
-function lookedUp(wordsFamiliarity: WordsFamiliarity, ...words: string[]): WordsFamiliarity {
-    return descreaseFamiliarity(wordsFamiliarity, words);
-}
-
-function notLookedUp(wordsFamiliarity: WordsFamiliarity, ...words): WordsFamiliarity {
-    return increaseFamiliarity(wordsFamiliarity, words);
-}
